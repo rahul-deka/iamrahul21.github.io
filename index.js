0b31f8f93bf32bffffa937c3707fad33a0f09edc
iamRahul21 <rahuldeka072@gmail.com>
2024-09-18 12:26:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMobileNavState() {
+    const a = document.querySelector("body");
+    a.classList.toggle("nav--active")
+}
+function initBurger() {
+    const a = document.querySelector(".burger");
+    a.addEventListener("click", toggleMobileNavState)
+}
+initBurger()
